dbb851960051c6945a6dcc5d372640e9b61d4462
m7lewandowska <marfalewandowska@gmail.com>
2020-05-13 11:36:18 +0200

    adapter

--- app/src/main/java/com/example/managemev2/Adapter.kt
+++ app/src/main/java/com/example/managemev2/Adapter.kt
@@ -3,26 +3,47 @@ package com.example.managemev2
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class Adapter: RecyclerView.Adapter<MyViewHolder>() {
+
+    val movieList = mutableListOf<Movie>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
         val  inflater: LayoutInflater = LayoutInflater.from(parent.context)
         val view: View  = inflater.inflate(R.layout.item_row, parent, false)
+        var row : LinearLayout = view.findViewById(R.id.single_row)
+
         return MyViewHolder(view)
 
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
+        return movieList.size();
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+//        TODO("Not yet implemented")
+
+         var movie : Movie = movieList[position]
+        holder.movieTitle.text = movie.title
+        holder.movieDirector.text = movie.director
+        holder.movieGenre.text = movie.genre
+        holder.movieYear.text = movie.productionYear
     }
 
+    inner class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+    {
+        val movieTitle : TextView = view.findViewById(R.id.MovieName)
+        val movieDirector : TextView = view.findViewById(R.id.MovieDirector)
+        val movieGenre : TextView = view.findViewById(R.id.MovieGenre)
+        val movieYear : TextView = view.findViewById(R.id.MovieProductionYear)
+
+    }
 }
 
-class MyViewHolder(val view: View): RecyclerView.ViewHolder(view)
+//class MyViewHolder(val view: View): RecyclerView.ViewHolder(view)
---
+++ app/src/main/java/com/example/managemev2/Movie.kt
@@ -0,0 +1,5 @@
+package com.example.managemev2
+
+class Movie {
+
+}
